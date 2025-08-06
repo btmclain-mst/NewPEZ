072312fcf3ecffc599406f2fbb046c539b4f821e
btmclain-mst <btmywv@umsystem.edu>
2025-08-06 16:03:06 -0500

    untested cavitar recording

--- offlineThreadCamera.py
+++ offlineThreadCamera.py
@@ -5,6 +5,7 @@ import cherrypy
 import numpy as np
 from pynput import keyboard
 import neoapi
+import os
 
 class USBVideoDisplay:
 	##### Video States
@@ -20,6 +21,7 @@ class USBVideoDisplay:
 		self.wiggleometer_flag = Value('i', 0)
 		self.fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 		self.video_source = video_source
+		self.record_state_flag = Value('i',0)
 
 		self.fps = 30
 		self.height = 1080
@@ -99,7 +101,11 @@ class CavitarVideoDisplay:
 		self.display_queue = Queue(maxsize=10)  # Queue to hold frames
 		self.record_queue = Queue(maxsize=600)
 		self.state_flag = Value('i', 0)
+		self.close_video = Value('b', False)  # Shared flag to stop processes
 		self.fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+		self.width=1440
+		self.height=1080
+		self.fps = 30
 
 
 	def capture_frames(self):
@@ -126,8 +132,7 @@ class CavitarVideoDisplay:
 		self.camera.f.LineMode.SetString('Output')
 		self.camera.f.LineSource.SetString('ExposureActive')
 
-		self.width=1440
-		self.height=1080
+
 		if self.camera.f.Width.IsWritable():
 			self.camera.f.Width.Set(self.width)
 		else:
@@ -180,12 +185,52 @@ class CavitarVideoDisplay:
 			cv2.waitKey(1)
 		cv2.destroyAllWindows() 
 
+	def record_frames(self):
+		name = str(time.time())
+		out = cv2.VideoWriter(name+'.mp4', self.fourcc, self.fps, (self.width, self.height))
+		while True:
+			if not self.record_queue.empty():
+				record_frame = self.record_queue.get()
+				if record_frame is not None: 
+					out.write(record_frame)
+				else:
+					print('empty frame recieved while recording')
+			
+			if self.close_video.value == True:
+				while not self.record_queue.empty():
+					record_frame = self.record_queue.get()
+					if record_frame is not None: 
+						out.write(record_frame)
+						print('dumping last frames')
+					else:
+						print('empty frame recieved while closing')
+
+				out.release()
+					
+				out = cv2.VideoWriter(name+'.mp4', self.fourcc, self.fps, (self.width, self.height))
+				self.close_video.value = False
+
+			if self.state_flag.value == 3:
+				if not self.record_queue.empty():
+					record_frame = self.record_queue.get()
+					out.write(record_frame)
+					print(f'dumping last frames')
+				out.release()
+				break
+
+			time.sleep(.01)
+
 def on_press(key):
 	global pressed_p
+	global pressed_r
+	global pressed_s
 	try:
 		if key.char == 'p':
 			pressed_p = True
-			return False  # stop listener after 'p'
+		if key.char == 'r':
+			pressed_r = True
+		if key.char == 'r':
+			pressed_s = True
 	except AttributeError:
 		if key == keyboard.Key.esc:
 			return False  # allow Esc to stop listener
@@ -204,22 +249,33 @@ if __name__ == "__main__":
 	cavitar_camera = CavitarVideoDisplay()
 	cavitar_capture_process = Process(target=cavitar_camera.capture_frames)
 	cavitar_display_process = Process(target=cavitar_camera.display_frames)
+	cavitar_record_process =  Process(target=cavitar_camera.record_frames)
 
 	cavitar_capture_process.start()
 	cavitar_display_process.start()
+	cavitar_record_process.start()
 	
 	listener = keyboard.Listener(on_press=on_press)
 	listener.start()
 
 	pressed_p = False
+
+	#CHECK FOR USER INPUTS
 	while True:
+		if pressed_r:
+			print(f'\nSTARTED RECORDING')
+			cavitar_camera.state_flag.value = 1
+		if pressed_s:
+			print(f'\nSTOPPED RECORDING')
+			cavitar_camera.state_flag.value = 0
+			cavitar_camera.close_video.value = True
 		if pressed_p:
-			print('\nClosing video')
-			#meltpool_camera.state_flag.value = 3
+			print('\nCLOSING')
+			meltpool_camera.state_flag.value = 3
 			cavitar_camera.state_flag.value = 3
 			
 			break
-		time.sleep(.001)
+		time.sleep(.01)
 
 
 
